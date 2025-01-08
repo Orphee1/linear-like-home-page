1d6c31fa8a0ade352cbae355763e8ad86462f101
Hugo Lattard <hugolattard@gmail.com>
2025-01-08 22:54:46 +0100

    fix bug 2

--- components/keyboard-shortcuts.tsx
+++ components/keyboard-shortcuts.tsx
@@ -22,7 +22,7 @@ const shortcuts = [
 const KeyboardShortcuts = () => {
   const wrapperRef = useRef<HTMLDivElement>(null)
   const illustrationWrapperRef = useRef<HTMLDivElement>(null)
-  const timeoutRef = useRef<NodeJS.Timeout>()
+  const timeoutRef = useRef<NodeJS.Timeout>(null)
   const activeShortCutIndex = useRef(0)
 
   const scheduleTimeout = () => {
